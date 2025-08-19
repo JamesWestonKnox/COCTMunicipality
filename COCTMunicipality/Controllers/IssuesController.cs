6e62eee62f763507d6468dabb910d0cfc1b2683e
James Knox <jamesknox361@gmail.com>
2025-08-19 14:37:03 +0200

    Added Issues, Announcements and status controllers

---
+++ COCTMunicipality/Controllers/IssuesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COCTMunicipality.Controllers
+{
+    public class IssuesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
