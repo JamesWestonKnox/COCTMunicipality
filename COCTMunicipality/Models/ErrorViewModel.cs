1c4758298bcee0b33f406cd5dd092e016a5f40cc
James Knox <jamesknox361@gmail.com>
2025-08-19 14:29:42 +0200

    Add project files.

---
+++ COCTMunicipality/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace COCTMunicipality.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
