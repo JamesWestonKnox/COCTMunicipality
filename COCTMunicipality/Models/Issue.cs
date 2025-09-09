61e9bc8b66494013249625b572c9a1b7006dd627
James Knox <jamesknox361@gmail.com>
2025-09-09 15:44:20 +0200

    Added Issue.cs, IssueNode.cs and IssueService.cs

---
+++ COCTMunicipality/Models/Issue.cs
@@ -0,0 +1,11 @@
+﻿namespace COCTMunicipality.Models
+{
+    public class Issue
+    {
+        public string Location { get; set; }
+        public string Category { get; set; }
+        public string Description { get; set; }
+        public string MediaFileName { get; set; }
+        public DateTime ReportedAt { get; set; } = DateTime.Now;
+    }
+}
---
+++ COCTMunicipality/Models/IssueNode.cs
@@ -0,0 +1,14 @@
+﻿namespace COCTMunicipality.Models
+{
+    public class IssueNode
+    {
+        public Issue Data { get; set; }
+        public IssueNode Next { get; set; }
+
+        public IssueNode(Issue data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+}
