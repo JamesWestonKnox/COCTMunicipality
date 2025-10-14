74522d60df9fa62cbe6c306037936a17a6e644f9
James Knox <jamesknox361@gmail.com>
2025-10-14 12:21:08 +0200

    Added announcement model

--- COCTMunicipality/Controllers/AnnouncementsController.cs
+++ COCTMunicipality/Controllers/AnnouncementsController.cs
@@ -1,13 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using COCTMunicipality.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace COCTMunicipality.Controllers
 {
     public class AnnouncementsController : Controller
     {
+
+        /// <summary>
+        /// Represents the service used to manage and interact with announcements.
+        /// </summary>
+        private readonly AnnouncementService announcementService;
+
+        public AnnouncementsController(AnnouncementService _announcementService)
+        {
+            announcementService = _announcementService;
+        }
+
+        /// <summary>
+        /// Displays the announcements page with all announcements.
+        /// </summary>
+        /// <returns>List of all announcements</returns>
         public IActionResult ViewAnnouncements()
         {
             ViewData["BodyClass"] = "index-page";
-            return View();
+            List<Announcement> allAnnouncements = announcementService.GetAllAnnouncements();
+            return View(allAnnouncements);
         }
     }
 }
---
+++ COCTMunicipality/Models/Announcement.cs
@@ -0,0 +1,15 @@
+﻿namespace COCTMunicipality.Models
+{
+    /// <summary>
+    /// Represents an announcement in the municipality.
+    /// </summary>
+    public class Announcement
+    {
+        public int AnnouncementId { get; set; }
+        public string Name { get; set; }
+        public string category { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+    }
+}
