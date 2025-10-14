b04a0311faee8453d60b4988e66a47c7f4237bee
James Knox <jamesknox361@gmail.com>
2025-10-14 12:47:21 +0200

    Announcement model now includes enum type to specificy whether it is an announcement or event

--- COCTMunicipality/Models/Announcement.cs
+++ COCTMunicipality/Models/Announcement.cs
@@ -1,7 +1,14 @@
 ﻿namespace COCTMunicipality.Models
 {
+    // Specify type to either event or announcement
+    public enum AnnouncementType
+    {
+        Event,
+        Announcement
+    }
+
     /// <summary>
-    /// Represents an announcement in the municipality.
+    /// Represents an announcement or event in the municipality.
     /// </summary>
     public class Announcement
     {
@@ -11,5 +18,6 @@
         public DateTime Date { get; set; }
         public string Location { get; set; }
         public string Description { get; set; }
+        public AnnouncementType Type { get; set; }
     }
 }
--- COCTMunicipality/Services/AnnouncementService.cs
+++ COCTMunicipality/Services/AnnouncementService.cs
@@ -1,10 +1,6 @@
-﻿using COCTMunicipality.Models;
-
-namespace COCTMunicipality.Services
+﻿namespace COCTMunicipality.Services
 {
     public class AnnouncementService
     {
-        // Dictionary to store announcements
-        private Dictionary<int, Announcement> announcements = new();
     }
 }
