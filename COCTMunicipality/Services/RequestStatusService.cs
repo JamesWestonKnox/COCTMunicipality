6a184dd5e127129c1ac7451d2408868e2e4994eb
James Knox <jamesknox361@gmail.com>
2025-11-12 10:24:01 +0200

    Completed RequestStatusService to populate advanced data structures with issues

---
+++ COCTMunicipality/Services/RequestStatusService.cs
@@ -0,0 +1,40 @@
+﻿/**
+ * RequestStatusService.cs
+ *
+ * Service class used to populate and initilize data structures for the request status page.
+ *
+ * Reference:
+ * OpenAI, 2025. ChatGPT [Computer program]. Version GPT-5.
+ * Available at: https://chat.openai.com
+ */
+
+using COCTMunicipality.Models;
+using COCTMunicipality.Models.DataStructures;
+
+namespace COCTMunicipality.Services
+{
+    public class RequestStatusService
+    {
+        private readonly RequestBinarySearchTree requestBinarySearchTree = new RequestBinarySearchTree();
+        private readonly RequestHeap requestHeap = new RequestHeap();
+        private readonly RequestGraph requestGraph = new RequestGraph();
+
+        public RequestStatusService(List<Issue> issues)
+        {
+            // Adds all issues to each data structure
+            foreach (var issue in issues)
+            {
+                requestBinarySearchTree.InsertRequest(issue);
+                requestHeap.Insert(issue);
+                requestGraph.AddRequestNode(issue);
+            }
+        }
+
+        public Issue SearchRequestByID(int id) => requestBinarySearchTree.SearchRequest(id);
+
+        public List<Issue> FetchAllRequestsByStatus() => requestHeap.GetAllRequestsOrderedByStatus();
+
+        public List<Issue> FetchRequestsByCategory(string category) => requestGraph.FetchRequestByCategory(category);
+
+    }
+}
